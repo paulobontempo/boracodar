9c13a8e9f769d2b25a5e8965c265c8a6e28956c7
Paulo Bontempo <paulobontempo@gmail.com>
2023-02-11 06:38:54 -0300

    feat: starting P002

---
+++ src/pages/P002.tsx
@@ -0,0 +1,10 @@
+export default function P002() {
+  return (
+    <div className="w-screen h-screen bg-P002bg">
+      <div className="flex flex-row justify-center items-center mx-32">
+        <div>IMAGEM</div>
+        <div>CÓDIGO: 42404</div>
+      </div>
+    </div>
+  )
+}
