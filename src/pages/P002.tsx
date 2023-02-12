6ee5ee7f3201719fa039c6357159096d93846584
Paulo Bontempo <paulobontempo@gmail.com>
2023-02-12 06:40:27 -0300

    feat: add sofa gif onCLick

--- src/pages/P002.tsx
+++ src/pages/P002.tsx
@@ -5,8 +5,11 @@ export default function P002() {
   const [showGif, setShowGif] = useState(false);
 
   const handleCLick = () => {
-    console.log('clicou')
     setShowGif(!showGif)
+  }
+
+  const handleButtonClick = () => {
+    alert('Button Clicked!')
   }
 
   return (
@@ -15,12 +18,12 @@ export default function P002() {
         {/* Sofa component */}
         <div>
           <div className="flex justify-end mr-16 h-[22px]">
-            {!showGif && <img src="/P002-360.png" onClick={handleCLick} alt="Clique para girar o sofá!" />}
-            {showGif && <X onClick={handleCLick} />}
+            {!showGif && <img src="/P002/P002-360.png" onClick={handleCLick} alt="Clique para girar o sofá!" />}
+            {showGif && <X className="mr-" size={22} color="#271A45" onClick={handleCLick} />}
           </div>
           <div>
-            {!showGif && <img src="/P002-sofa.png" alt="Sofa" />}
-            {showGif && <img src="/P002-sofa.png" alt="Sofa" />}
+            {!showGif && <img src="/P002/P002-sofa.png" alt="Sofa" />}
+            {showGif && <img className="max-h-[256px] max-w-64" src="/P002/P002-sofa.gif" alt="Sofa" />}
           </div>
         </div>
         {/* Info Component */}
@@ -28,7 +31,7 @@ export default function P002() {
           <div className="text-[10px] font-light font-lato">CÓDIGO: 42404</div>
           <div className="mt-3 text-3xl font-crimsonPro font-semibold">Sofá Margot II - Rosé</div>
           <div className="mt-3 font-normal text-base font-lato">R$ 4.000 </div>
-          <div className="w-2/3 mt-3 border border-P002Heading rounded-full px-4 py-2">ADICIONAR À CESTA</div>
+          <button onClick={handleButtonClick} className="w-2/3 mt-3 hover:bg-violet-300 active:bg-violet-400 focus:outline-none focus:ring focus:ring-violet-300  border border-P002Heading rounded-full px-4 py-2">ADICIONAR À CESTA</button>
         </div>
       </div>
     </div>
