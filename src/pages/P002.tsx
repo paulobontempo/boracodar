a82f6b50c9cebab04997749accfcd72b5eb05816
Paulo Bontempo <paulobontempo@gmail.com>
2023-02-12 06:42:14 -0300

    feat: goBack button for P002

--- src/pages/P002.tsx
+++ src/pages/P002.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { X } from "phosphor-react";
 import { useState } from "react"
 
@@ -13,27 +14,32 @@ export default function P002() {
   }
 
   return (
-    <div className="w-screen h-screen bg-P002bg">
-      <div className="flex justify-center items-center h-screen">
-        {/* Sofa component */}
-        <div>
-          <div className="flex justify-end mr-16 h-[22px]">
-            {!showGif && <img src="/P002/P002-360.png" onClick={handleCLick} alt="Clique para girar o sofá!" />}
-            {showGif && <X className="mr-" size={22} color="#271A45" onClick={handleCLick} />}
-          </div>
-          <div>
-            {!showGif && <img src="/P002/P002-sofa.png" alt="Sofa" />}
-            {showGif && <img className="max-h-[256px] max-w-64" src="/P002/P002-sofa.gif" alt="Sofa" />}
-          </div>
+    <>
+      <div className="bg-P002bg text-red-600">
+          <Link href="/">VOLTAR</Link>
         </div>
-        {/* Info Component */}
-        <div className="text-P002Heading">
-          <div className="text-[10px] font-light font-lato">CÓDIGO: 42404</div>
-          <div className="mt-3 text-3xl font-crimsonPro font-semibold">Sofá Margot II - Rosé</div>
-          <div className="mt-3 font-normal text-base font-lato">R$ 4.000 </div>
-          <button onClick={handleButtonClick} className="w-2/3 mt-3 hover:bg-violet-300 active:bg-violet-400 focus:outline-none focus:ring focus:ring-violet-300  border border-P002Heading rounded-full px-4 py-2">ADICIONAR À CESTA</button>
+      <div className="w-screen h-screen bg-P002bg">
+        <div className="flex justify-center items-center h-screen">
+          {/* Sofa component */}
+          <div>
+            <div className="flex justify-end mr-16 h-[22px]">
+              {!showGif && <img src="/P002/P002-360.png" onClick={handleCLick} alt="Clique para girar o sofá!" />}
+              {showGif && <X className="mr-" size={22} color="#271A45" onClick={handleCLick} />}
+            </div>
+            <div>
+              {!showGif && <img src="/P002/P002-sofa.png" alt="Sofa" />}
+              {showGif && <img className="max-h-[256px] max-w-64" src="/P002/P002-sofa.gif" alt="Sofa" />}
+            </div>
+          </div>
+          {/* Info Component */}
+          <div className="text-P002Heading">
+            <div className="text-[10px] font-light font-lato">CÓDIGO: 42404</div>
+            <div className="mt-3 text-3xl font-crimsonPro font-semibold">Sofá Margot II - Rosé</div>
+            <div className="mt-3 font-normal text-base font-lato">R$ 4.000 </div>
+            <button onClick={handleButtonClick} className="w-2/3 mt-3 hover:bg-violet-300 active:bg-violet-400 focus:outline-none focus:ring focus:ring-violet-300  border border-P002Heading rounded-full px-4 py-2">ADICIONAR À CESTA</button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
